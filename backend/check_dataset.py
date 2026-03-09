c8fd01989e3e35f87a3f8f264415431fc411372f
Harsh2111vardhan <harsh2111vardhan@gmail.com>
2026-03-09 19:05:50 +0530

    Remove dev scripts and pycache

--- backend/check_dataset.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("app/data/symptom2disease.csv")
-
-print(df.columns)
-print(df.head())
--- backend/test.py
+++
@@ -1,6 +0,0 @@
-import joblib
-
-cols = joblib.load("backend/app/data/symptom_columns.pkl")
-
-print(len(cols))
-print(cols[:30])
